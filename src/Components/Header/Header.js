91e9b3d5db89cfd04a43a049a4e4b6d4d394e381
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-27 00:14:07 +0530

    most of the work done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,9 +5,9 @@ import Cart from '../Cart/Cart';
 const header = () => {
   return (
     <div>
-      <Navbar className='' bg="dark" variant="dark" expand="lg">
+      <Navbar className="" bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Medicine-Store</Navbar.Brand>
+          <Navbar.Brand href="#home">Shoes Shop for kids </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
